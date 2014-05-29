4e1133a2f342906ce086ed67453bac50c04b340f
Dan Carlstedt <dancarlstedt@gmail.com>
2014-05-28 23:13:01 -0500

    Adding mongoose stuff and default blog

--- server.js
+++ server.js
@@ -1,73 +1,119 @@
 var express = require('express');
 var logger = require('morgan');
-var _ = require('underscore');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
 
 var config = {
-    port: 3000
+    port: 3000,
+    connection: 'mongodb://localhost:27017/blogger'
 };
 
-var blogs = [
-    {
-        _id_: 1,
-        title: "AngularJS",
-        date: new Date(),
-        author: 'Dan Carlstedt',
-        image: 'angular.png',
-        comments: [
-            {
-                username: 'Steve Smith',
-                date: new Date(),
-                message: 'Thank you that was a kick ass posting!!'
-            }
-        ]
-    }
-];
+// main config
 
-// ****** Router Stuff *****
-// static assets only
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
+// db config
+
+var db = mongoose.createConnection(config.connection);
+db.on('error', console.error.bind(console, 'connection error'));
+db.on('open', function () {
+    console.log('connection successful');
+});
+
+// blog model and init
+var blogSchema = new mongoose.Schema({
+    title: String,
+    date: Date,
+    author: String,
+    image: String,
+    comments: [
+        {username: String, date: Date, message: String}
+    ]
+});
+
+var Blog = db.model('Blog', blogSchema);
+Blog.findOne(function (err, blog) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    if (!blog) {
+        console.log('no blog found... creating default entry');
+        var defaultBlog = new Blog({
+            title: "AngularJS",
+            date: new Date(),
+            author: 'Dan Carlstedt',
+            image: 'angular.png',
+            comments: [
+                {
+                    username: 'Steve Smith',
+                    date: new Date(),
+                    message: 'Thank you that was a kick ass posting!!'
+                }
+            ]
+        });
+        defaultBlog.save();
+    } else {
+        console.log('we found a blog');
+        console.log(blog);
+    }
+});
+
+// blog routes
 var router = express.Router();
 router.use(logger());
 
 router.route('/blogs')
     .get(function (req, res) {
-        res.json(blogs);
+        Blog.find(function (err, blogs) {
+            res.json(blogs);
+        });
     })
     .post(function (req, res) {
-        var maxPosting = _.max(blogs, function (blog) {
-            return blog._id_;
+        var body = req.body;
+        var newBlog = new Blog({
+            title: body.title,
+            date: new Date(),
+            author: body.author,
+            image: body.image,
+            comments: body.comments
         });
 
-        var newBlog = {_id_: maxPosting._id_ + 1};
-        var body = req.body;
-        newBlog.title = body.title;
-        newBlog.date = new Date();
-        newBlog.author = body.author;
-        newBlog.image = body.imag;
-        newBlog.comments = body.comments;
-
-
-        blogs.push(newBlog);
-
-        res.status(201).json(newBlog);
+        newBlog.save(function (err, newBLog) {
+            if (err) res.status(500).json(err);
+            res.status(201).json(newBlog);
+        });
     });
 
 router.route('/blogs/:blog_id')
     .get(function (req, res) {
         var blogId = req.params.blog_id;
-        var blog = _.find(blogs, function (blog) {
-            return blog._id_ == blogId;
+        Blog.findById(blogId, function (err, blog) {
+            if(err) res.status(500).json(err);
+            if(blog) {
+                res.json(blog);
+            }else{
+                res.status(404).json({message :"Id not found: " + blogId});
+            }
+        });
+    })
+    .put(function (req, res) {
+        var blogId = req.params.blog_id;
+        Blog.findById(blogId, function (err, blog) {
+            if(err) res.status(500).json(err);
+            if(blog){
+                // TODO: upate the blog here
+            }else{
+                res.status(404).json({message :"Id not found: " + blogId});
+            }
         })
-        res.json(blog);
     });
 
 app.use('/api', router);
 
-// **** End Router Stuff ****
 
 app.listen(config.port);
 console.log('Express started on port ' + config.port);
